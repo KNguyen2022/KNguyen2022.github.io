3ab0057b2f7856c5230c0a7683225b1bfaab7882
KNguyen2022 <knguye71@uncc.edu>
2022-09-25 11:56:47 -0400

    WIP activity#5

---
+++ itis3135/scripts/defaultScript.js
@@ -0,0 +1,8 @@
+window.alert("Hey my script is running");
+
+
+// function dateAndTime(){
+//     let time= document.getElementById("date-time");
+//     let dt = new Date();
+//     time.innerHTML = dt.toLocaleTimeString();
+// }
