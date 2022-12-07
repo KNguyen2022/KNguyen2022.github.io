097674ef2bb478e63542c1b44f4457652ac9896a
KNguyen2022 <knguye71@uncc.edu>
2022-12-07 18:45:11 -0500

    -Update 12/7

--- itis3135/scripts/defaultScript.js
+++ itis3135/scripts/defaultScript.js
@@ -3,9 +3,11 @@ function scriptTest(){
     alert("Hey my script is running");
 }
 // Welcome Message
-    let input = prompt("What is your name?");
-    let greeting = "Welcome " + input + "!";
+    let nameInput = prompt("What is your name?");
+    let emotionInput = prompt("How are you feeling?");
+    let greeting = "Welcome " + nameInput + "!";
     document.getElementById("greeting").innerHTML = greeting;
+    let greetMsg = "KNTech Welcomes you, " + nameInput + "!\nWe're glad you are doing " + emotionInput + "!";
 
 // Incrementing Clock
     setInterval(() =>{
